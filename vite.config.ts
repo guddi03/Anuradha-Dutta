8a1fdfd43f66d09d028eee24613bbcef355bcf8c
Anushka dutta <113164826+guddi03@users.noreply.github.com>
2025-10-25 20:28:36 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,10 @@ import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ mode }) => {
+export default defineConfig(({ export default defineConfig({
+  base: '/<your-repo-name>/',
+});
+ }) => {
     const env = loadEnv(mode, '.', '');
     return {
       server: {
